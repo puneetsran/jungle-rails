f1e458c0e90b32812eb87876a4f436ee0b100d9d
Puneet Sran <puneet_sran@icloud.com>
2019-11-18 21:07:29 +0000

    setup rspec

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+# RSpec.describe Product, type: :model do
+#   pending "add some examples to (or delete) #{__FILE__}"
+# end
+
+RSpec.describe Product, type: :model do
+  describe 'Validations' do
+    # validation tests/examples here
+  end
+end
