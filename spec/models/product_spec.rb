46eae9afac17c08278b31cebb7a7912ec6164190
Puneet Sran <puneet_sran@icloud.com>
2019-11-18 23:33:49 +0000

    implement validation specs for product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,11 +1,34 @@
 require 'rails_helper'
 
-# RSpec.describe Product, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
-
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    # validation tests/examples here
+    before(:each) do
+      category = Category.create(name: "Fitness")
+      @product = category.products.new(name: "Skipping Rope", price_cents: 120, quantity: 20)
+    end
+
+    it "Validates all fields" do
+      expect(@product).to be_valid
+    end
+
+    it "validates :name" do
+      @product.name = nil
+      expect(@product).to_not be_valid
+    end
+
+    it "validates :price" do
+      @product.price_cents = nil
+      expect(@product).to_not be_valid
+    end
+
+    it "validates :quantity" do
+      @product.quantity = nil
+      expect(@product).to_not be_valid
+    end
+
+    it "validates :category" do
+      @product.category = nil
+      expect(@product).to_not be_valid
+    end
   end
 end
