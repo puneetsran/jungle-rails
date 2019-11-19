098dedc33789584ff02294a107783c3eb1def77b
Puneet Sran <puneet_sran@icloud.com>
2019-11-19 00:05:03 +0000

    add validats for login criteria and define authentication method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates: email, uniqueness: {case_sensitive: false}, presence: true
+  validates: name, presence: true
+  validates: password, length: { in: 6..15}, presence: true
+  validates: password_confirmation, presence: true
+  
+  def self.authenticate_with_credentials(email, password)
+    user = User.find_by_email(email.downcase.strip)
+    if user && user.authenticate(password)
+      user
+    else
+      nil
+  end
 end
